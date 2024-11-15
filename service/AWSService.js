33a68d51594cb54c241027281605070b39c8956f
Dayo Akinbami <dayoakinbami@Dayos-MacBook-Pro.local>
2024-11-15 14:34:31 +0100

    load secrets

--- service/AWSService.js
+++ service/AWSService.js
@@ -3,16 +3,15 @@ const aws = require("aws-sdk");
 const fs = require("fs");
 const multer = require("multer");
 const multerS3 = require("multer-s3");
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
 const BUCKET_NAME = "the-pod-profile-pictures";
 
 aws.config.update({
-    accessKeyId: "AKIAQHWIICMX6QYTCN53",
-    secretAccessKey: "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",
-    region: "us-east-1"
-  });
-
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+  region: process.env.AWS_REGION,
+});
 
 const s3 = new aws.S3();
 // const params = {
@@ -22,76 +21,75 @@ const s3 = new aws.S3();
 const filename = uuidv4();
 
 const upload = multer({
-    storage: multerS3({
-        s3,
-        bucket: BUCKET_NAME,
-        contentType: multerS3.AUTO_CONTENT_TYPE,
-        acl: 'public-read',
-        metadata: function (req, file, cb){
-            cb(null, {fieldName: file.fieldname})
-        },
-        key: function (req, file, cb){
-            cb(null, filename)
-        }
-    })
-})
+  storage: multerS3({
+    s3,
+    bucket: BUCKET_NAME,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: "public-read",
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function (req, file, cb) {
+      cb(null, filename);
+    },
+  }),
+});
 
 const singleFileUpload = upload.single("file");
 
 const AZ = {
-    async uploadFile(file){
-        const fileContent = fs.readFileSync(file.path);
+  async uploadFile(file) {
+    const fileContent = fs.readFileSync(file.path);
 
-        console.log("file content: ",fileContent, file.path)
-        // Setting up S3 upload parameters
-        const params = {
-            Bucket: BUCKET_NAME,
-            Key: file.filename, // File name you want to save as in S3
-            Body: fileContent
-        };
+    console.log("file content: ", fileContent, file.path);
+    // Setting up S3 upload parameters
+    const params = {
+      Bucket: BUCKET_NAME,
+      Key: file.filename, // File name you want to save as in S3
+      Body: fileContent,
+    };
 
-        // Uploading files to the bucket
+    // Uploading files to the bucket
 
-        console.log("bucket params: ",params)
-        // let response;
-        return s3.upload(params).promise();
+    console.log("bucket params: ", params);
+    // let response;
+    return s3.upload(params).promise();
 
-        // s3.getSignedUrl("putObject", params, function(err, data) {
-        //     if (err) {
-        //         console.log(err);
-        //         return err;
-        //     } else {
-        //         console.log(`File uploaded successfully. ${data}`);
-        //         return {'success': true, message: 'File uploaded to Amazon S3.', url: data };
-        //     }
-        // });
+    // s3.getSignedUrl("putObject", params, function(err, data) {
+    //     if (err) {
+    //         console.log(err);
+    //         return err;
+    //     } else {
+    //         console.log(`File uploaded successfully. ${data}`);
+    //         return {'success': true, message: 'File uploaded to Amazon S3.', url: data };
+    //     }
+    // });
 
-        // var putObjectPromise = s3.putObject(params).promise();
-        // putObjectPromise.then(function(data) {
-        //   console.log("Successfully uploaded data to " + params.Bucket + "/" + params.Key);
-        //   return {'success': true, message: 'File uploaded to Amazon S3.', url: data };
-        // }).catch(err=>console.error(err));
+    // var putObjectPromise = s3.putObject(params).promise();
+    // putObjectPromise.then(function(data) {
+    //   console.log("Successfully uploaded data to " + params.Bucket + "/" + params.Key);
+    //   return {'success': true, message: 'File uploaded to Amazon S3.', url: data };
+    // }).catch(err=>console.error(err));
 
-        // s3.putObject(params, function (err, data) {
-        //     if (err) {
-        //         console.log("Error: ", err);
-        //         return err;
-        //     } else {
-        //         console.log(`File uploaded successfully. ${data}`);
-        //         return {'success': true, message: 'File uploaded to Amazon S3.', url: data };
-        //     }
-        // });
-
-    },
-    async imageUpload(req,res){
-        let downloaduri  = `https://${BUCKET_NAME}.s3.amazonaws.com/${filename}`
-        return new Promise((resolve, reject)=>{
-            return singleFileUpload(req,res,err=>{
-                if (err) return reject(err);
-                return resolve(downloaduri)
-            })
-        })
-    }
-}
+    // s3.putObject(params, function (err, data) {
+    //     if (err) {
+    //         console.log("Error: ", err);
+    //         return err;
+    //     } else {
+    //         console.log(`File uploaded successfully. ${data}`);
+    //         return {'success': true, message: 'File uploaded to Amazon S3.', url: data };
+    //     }
+    // });
+  },
+  async imageUpload(req, res) {
+    let downloaduri = `https://${BUCKET_NAME}.s3.amazonaws.com/${filename}`;
+    return new Promise((resolve, reject) => {
+      return singleFileUpload(req, res, (err) => {
+        if (err) return reject(err);
+        return resolve(downloaduri);
+      });
+    });
+  },
+};
 
 module.exports = AZ;
--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,104 +1,107 @@
-const Mailchimp = require('mailchimp-api-v3');
-const mailchimp = new Mailchimp("c3f6755eac894252e3afe117aa086caf-us6");
+const Mailchimp = require("mailchimp-api-v3");
+const mailchimp = new Mailchimp(process.env.MAILCHIMP_KEY);
 const nodemailer = require("nodemailer");
-var SibApiV3Sdk = require('sib-api-v3-sdk');
+var SibApiV3Sdk = require("sib-api-v3-sdk");
 const axios = require("axios");
 
-
-
-const send_in_blue = "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";
-const sendgrid_api_key = "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"
-
-
+const send_in_blue = process.env.SEND_IN_BLUE_KEY;
+const sendgrid_api_key = process.env.SENDGRID_API_KEY;
 
 // Uncomment below two lines to configure authorization using: partner-key
 // var partnerKey = defaultClient.authentications['partner-key'];
 // partnerKey.apiKey = 'YOUR API KEY';
 
 const sendEmail = async (mailObj) => {
-    const { from, recipient, subject, message, firstname, lastname, roomnumber } = mailObj;
+  const { from, recipient, subject, message, firstname, lastname, roomnumber } =
+    mailObj;
 
-    try {
-        const defaultClient = SibApiV3Sdk.ApiClient.instance;
+  try {
+    const defaultClient = SibApiV3Sdk.ApiClient.instance;
 
-        // Configure API key authorization: api-key
-        var apiKey = defaultClient.authentications['api-key'];
-        apiKey.apiKey = send_in_blue;
+    // Configure API key authorization: api-key
+    var apiKey = defaultClient.authentications["api-key"];
+    apiKey.apiKey = send_in_blue;
 
-        var apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+    var apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 
-        var sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail(); // SendSmtpEmail | Values to send a transactional email
+    var sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail(); // SendSmtpEmail | Values to send a transactional email
 
-        sendSmtpEmail = {
-            to: [{
-                email: recipient,
-                name: `${firstname} ${lastname}`
-            }],
-            templateId: 1,
-            params: {
-                name: firstname,
-                surname: lastname,
-                roomnumber,
-                recipient
-            },
-            headers: {
-                'X-Mailin-custom': 'custom_header_1:custom_value_1|custom_header_2:custom_value_2'
-            }
-        };
+    sendSmtpEmail = {
+      to: [
+        {
+          email: recipient,
+          name: `${firstname} ${lastname}`,
+        },
+      ],
+      templateId: 1,
+      params: {
+        name: firstname,
+        surname: lastname,
+        roomnumber,
+        recipient,
+      },
+      headers: {
+        "X-Mailin-custom":
+          "custom_header_1:custom_value_1|custom_header_2:custom_value_2",
+      },
+    };
 
-        console.log("email payload: ", sendSmtpEmail)
+    console.log("email payload: ", sendSmtpEmail);
 
-        const post = await axios.post("https://api.sendinblue.com/v3/smtp/email",sendSmtpEmail,{
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json",
-                "api-key": send_in_blue
-            }
-        });
+    const post = await axios.post(
+      "https://api.sendinblue.com/v3/smtp/email",
+      sendSmtpEmail,
+      {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          "api-key": send_in_blue,
+        },
+      }
+    );
 
-        console.log("response from API, ", post.data)
-        
-        return post.data
+    console.log("response from API, ", post.data);
 
-        // apiInstance.sendTransacEmail(sendSmtpEmail).then(function(data) {
-        //     console.log('API called successfully. Returned data: ' + data);
-        //     return data
-        // }, function(error) {
-        //     console.error("and error happed",error);
-        //     throw new Error(error)
-        // }).catch(err=>{
-        //     console.log("an error occured", error)
-        // });
+    return post.data;
 
-        // console.log("after instance: ")
-        // const sgMail = require('@sendgrid/mail');
-        // sgMail.setApiKey(sendgrid_api_key)
-        // console.log("this is sendgrid api key: ", sendgrid_api_key)
-        // const msg = {
-        //     to: recipient, // Change to your recipient
-        //     from: 'enquiries@smartpysolutions.com', // Change to your verified sender
-        //     templateId: "d-410a99fef6ca44098b359d6a7eec8505",
-        //     dynamic_template_data: {
-        //         firstname: firstname,
-        //         room:  roomnumber,
-        //     }
-        // }
-        // sgMail
-        // .send(msg)
-        // .then((data) => {
-        //     console.log('Email sent');
-        //     return data
-        // }).catch((error) => {
-        //     console.error("error email: ",error)
-        //     throw new Error(error)
-        // })
-    } catch (error) {
-        console.error("Something went wrong in the sendmail method.", error);
-        throw new Error(
-            `Something went wrong in the sendmail method. Error: ${error.message}`
-        );
-    }
+    // apiInstance.sendTransacEmail(sendSmtpEmail).then(function(data) {
+    //     console.log('API called successfully. Returned data: ' + data);
+    //     return data
+    // }, function(error) {
+    //     console.error("and error happed",error);
+    //     throw new Error(error)
+    // }).catch(err=>{
+    //     console.log("an error occured", error)
+    // });
+
+    // console.log("after instance: ")
+    // const sgMail = require('@sendgrid/mail');
+    // sgMail.setApiKey(sendgrid_api_key)
+    // console.log("this is sendgrid api key: ", sendgrid_api_key)
+    // const msg = {
+    //     to: recipient, // Change to your recipient
+    //     from: 'enquiries@smartpysolutions.com', // Change to your verified sender
+    //     templateId: "d-410a99fef6ca44098b359d6a7eec8505",
+    //     dynamic_template_data: {
+    //         firstname: firstname,
+    //         room:  roomnumber,
+    //     }
+    // }
+    // sgMail
+    // .send(msg)
+    // .then((data) => {
+    //     console.log('Email sent');
+    //     return data
+    // }).catch((error) => {
+    //     console.error("error email: ",error)
+    //     throw new Error(error)
+    // })
+  } catch (error) {
+    console.error("Something went wrong in the sendmail method.", error);
+    throw new Error(
+      `Something went wrong in the sendmail method. Error: ${error.message}`
+    );
+  }
 };
 
-module.exports = {sendEmail}
-
+module.exports = { sendEmail };
